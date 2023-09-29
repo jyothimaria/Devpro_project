2ccf13c9401bb30946e25ba2ef3297f8d3e04f5d
jyothimaria <122686121+jyothimaria@users.noreply.github.com>
2023-09-28 22:16:11 -0400

    Update test_classification_project.py

--- tests/test_classification_project.py
+++ tests/test_classification_project.py
@@ -3,7 +3,6 @@ import tensorflow as tf
 import numpy as np
 from data_preparation import load_cifar10_data
 from model import create_cnn_model
-from train import train_model
 from predict import predict_image
 
 @pytest.fixture
@@ -15,7 +14,6 @@ def cifar10_data():
 def cifar10_model(cifar10_data):
     x_train, y_train, _, _ = cifar10_data
     model = create_cnn_model(x_train[0].shape)
-    train_model(model, x_train, y_train, epochs=1, batch_size=64)
     return model
 
 def test_data_preparation(cifar10_data):
